ca2dd9b7f1348616d0e000ce4202cb1c3bf4df29
Dmytro <dbogatov@wpi.edu>
2016-10-11 22:31:28 -0400

    Adit info.

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,7 @@ var callback = function () {
 			height: 14,
 		}, 2000);
 	});
+	$("#age").text(age(new Date(1991, 11, 25)) + "yo");
 };
 $(document).ready(callback);
 
@@ -22,3 +23,9 @@ window.onresize = function () {
 		callback();
 	}, 100);
 };
+
+var age = function (birthday) { // birthday is a date
+	var ageDifMs = Date.now() - birthday.getTime();
+	var ageDate = new Date(ageDifMs); // miliseconds from epoch
+	return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
