70e3c16c55c5938fe2260f4e2598b36ac0e9fdc9
Dmytro <dbogatov@wpi.edu>
2017-02-13 19:34:30 -0500

    Add CI and Docker.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing node modules... Requires NPM"
+npm install --loglevel=error > /dev/null
+
+echo "Installing Bower libraries... Requires Bower (installed by NPM)"
+$(npm bin)/bower install > /dev/null
+
+echo "Build completed!"
